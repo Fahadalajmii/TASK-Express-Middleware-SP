10d27cc6a189bd451f5364f7e9abc39b50af9d18
fahad alajmi <fahadalajmi126@gmail.com>
2022-02-08 18:10:23 +0300

    done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const connectDb = require('./database');
-const productsRoutes = require('./api/products/routes');
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./database");
+const productsRoutes = require("./api/products/routes");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 connectDb();
@@ -9,7 +11,22 @@ connectDb();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.method, req.protocol, req.hostname, req.originalUrl);
+  next();
+});
 // Routes
-app.use('/products', productsRoutes);
+app.use("/products", productsRoutes);
+
+//error handling middleware
+app.use((error, req, res, next) => {
+  return res
+    .status(error.status || 500)
+    .json(error.message || "enternal server error");
+});
+//not found middle ware
+app.use((req, res, next) => {
+  return res.status(404).json("not found path");
+});
 
 app.listen(process.env.PORT || 5000);
